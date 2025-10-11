56f1fbf10bee4e888eaa74eb0108ab6846353870
remusito <rcerezomartin@gmail.com>
2025-10-11 17:49:56 +0200

    Update client-provider.tsx

--- src/firebase/client-provider.tsx
+++ src/firebase/client-provider.tsx
@@ -1,10 +1,10 @@
 'use client';
 
-import { initializeFirebase, FirebaseProvider } from '@/firebase';
-import { firebaseConfig } from '@/firebase/config';
+import { FirebaseProvider } from '@/firebase';
 import { useMemo, type ReactNode } from 'react';
 
 export function FirebaseClientProvider({ children }: { children: ReactNode }) {
-  const value = useMemo(() => initializeFirebase(firebaseConfig), []);
+  const value = useMemo(() => ({}), []);
+  
   return <FirebaseProvider value={value}>{children}</FirebaseProvider>;
 }
